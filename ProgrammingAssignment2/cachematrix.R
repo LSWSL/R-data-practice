e333890d504db1bbfa6682079bc38c3b22c3edf2
刘盛炜 <58833670+LSWSL@users.noreply.github.com>
2020-10-16 00:21:14 +0800

    first

---
+++ ProgrammingAssignment2/cachematrix.R
@@ -0,0 +1,32 @@
+## At first, we initialize two global variable, they cache the matrix that input
+## before, and it's inverse.
+cachematrix<<-NULL
+cacheinverse<<-NULL
+
+##The function be use to make the cache list as the input of the second function
+makeCacheMatrix <- function(x = matrix()) {
+  get<-x
+  maintain<-cachematrix
+  maintaininverse<-cacheinverse
+  
+  list(get=get,maintain=maintain,
+        maintaininverse=maintaininverse)
+}
+
+
+## The function test if the input equal to the matrix that input before, and if 
+## not caculate the inverse.
+
+cacheSolve <- function(x, ...) {
+  m<-x$maintaininverse
+  if(!is.null(m)){
+    if(all(x$maintain==x$get)){
+      message("getting cached data")
+      return(m)
+      }
+  }
+  ma<-x$get
+  cacheinverse<<-solve(ma)
+  cachematrix<<-ma
+  cacheinverse      
+}
